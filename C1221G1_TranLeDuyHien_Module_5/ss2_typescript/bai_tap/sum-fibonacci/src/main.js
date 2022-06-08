2c7080fcb4ff7efcf429b3ed04c98775ca402099
duyhien2410 <hienduy2410@gmail.com>
2022-06-08 21:35:11 +0700

    up

--- C1221G1_TranLeDuyHien_Module_5/ss2_typescript/bai_tap/sum-fibonacci/src/main.js
+++ C1221G1_TranLeDuyHien_Module_5/ss2_typescript/bai_tap/sum-fibonacci/src/main.js
@@ -1,39 +1,38 @@
-// let a=0 , b=0 , c=0 , sum=0;
-// let fibonacci="";
-// for (let i=1; i<=20; i++){
-//     if (i==1){
-//         a=1
-//         c=1
-//         fibonacci += a + ", "
-//         sum+=a
-//     } else {
-//         a=b+c
-//         b=c
-//         c=a
-//         fibonacci += a + ", "
-//         sum+=a
-//     }
-// }
-// console.log(fibonacci);
-// console.log("Tổng dãy fibonacci = "+sum);
-function isFibonacci(number) {
+function sumFibonacci(number) {
     var a = 0, b = 0, c = 0, sum = 0;
-    var fibonacci = "";
     for (var i = 1; i <= number; i++) {
         if (i == 1) {
             a = 1;
             c = 1;
-            fibonacci += a + ", ";
             sum += a;
         }
         else {
             a = b + c;
             b = c;
             c = a;
-            fibonacci += a + ", ";
             sum += a;
         }
     }
-    return fibonacci + ("Tổng dãy fibonacci = " + sum);
+    return sum;
 }
-console.log(isFibonacci(20));
+function fibonacci(number) {
+    var a = 0, b = 0, c = 0;
+    var fibo = "";
+    for (var i = 1; i <= number; i++) {
+        if (i == 1) {
+            a = 1;
+            c = 1;
+            fibo += a + ", ";
+        }
+        else {
+            a = b + c;
+            b = c;
+            c = a;
+            fibo += a + ", ";
+        }
+    }
+    return fibo;
+}
+var num = 20;
+console.log(fibonacci(num));
+console.log("Tổng dãy fibonacci = " + sumFibonacci(num));
--- C1221G1_TranLeDuyHien_Module_5/ss2_typescript/bai_tap/sum-fibonacci/src/main.ts
+++ C1221G1_TranLeDuyHien_Module_5/ss2_typescript/bai_tap/sum-fibonacci/src/main.ts
@@ -1,40 +1,38 @@
-// let a=0 , b=0 , c=0 , sum=0;
-// let fibonacci="";
-// for (let i=1; i<=20; i++){
-//     if (i==1){
-//         a=1
-//         c=1
-//         fibonacci += a + ", "
-//         sum+=a
-//     } else {
-//         a=b+c
-//         b=c
-//         c=a
-//         fibonacci += a + ", "
-//         sum+=a
-//     }
-// }
-// console.log(fibonacci);
-// console.log("Tổng dãy fibonacci = "+sum);
-
-function isFibonacci(number : number) {
+function sumFibonacci(number : number) {
     let a = 0, b = 0, c = 0, sum = 0;
-    let fibonacci="";
     for (let i = 1; i <= number; i++) {
         if (i == 1) {
             a = 1
             c = 1
-            fibonacci += a + ", "
             sum += a
         } else {
             a = b + c
             b = c
             c = a
-            fibonacci += a + ", "
             sum += a
         }
     }
-    return fibonacci+("Tổng dãy fibonacci = "+sum);
+    return sum;
 }
 
-console.log(isFibonacci(20));
+function fibonacci(number : number) {
+    let a = 0, b = 0, c = 0;
+    let fibo = "";
+    for (let i = 1; i <= number; i++) {
+        if (i == 1) {
+            a = 1
+            c = 1
+            fibo += a + ", "
+        } else {
+            a = b + c
+            b = c
+            c = a
+            fibo += a + ", "
+        }
+    }
+    return fibo;
+}
+
+let num: number = 20;
+console.log(fibonacci(num))
+console.log("Tổng dãy fibonacci = "+sumFibonacci(num));
