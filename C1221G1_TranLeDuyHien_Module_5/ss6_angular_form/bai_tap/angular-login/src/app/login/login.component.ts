4f049bb18d48295ad603983ef9af31229a89ac9b
duyhien2410 <hienduy2410@gmail.com>
2022-06-13 17:29:57 +0700

    up ss6

---
+++ C1221G1_TranLeDuyHien_Module_5/ss6_angular_form/bai_tap/angular-login/src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {Login} from '../login';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  logins: Login[] = [];
+  loginForm: FormGroup;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+}
