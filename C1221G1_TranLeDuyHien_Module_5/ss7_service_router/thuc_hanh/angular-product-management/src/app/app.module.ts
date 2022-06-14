ce0202d31fa749697c400cbe58e4c80cc87d1218
duyhien2410 <hienduy2410@gmail.com>
2022-06-14 22:02:51 +0700

    up ss7

--- C1221G1_TranLeDuyHien_Module_5/ss7_service_router/thuc_hanh/angular-product-management/src/app/app.module.ts
+++ C1221G1_TranLeDuyHien_Module_5/ss7_service_router/thuc_hanh/angular-product-management/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { ProductCreateComponent } from './product/product-create/product-create.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import { ProductDeleteComponent } from './product/product-delete/product-delete.component';
+import { ProductEditComponent } from './product/product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductDeleteComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
